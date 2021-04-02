2ac9cbe42c1a44fe2aad69dfd178bfcaea674d74
Alberto Macias <alberto.macbar9512@gmail.com>
2021-04-01 20:13:22 -0600

    Code for events and screen refactored

--- allien_invasion.py
+++ allien_invasion.py
@@ -22,17 +22,22 @@ class AllienInvasion:
     def run_game(self):
         """Star the main loop for the game"""
         while True:
-            # Watch for kearboard and mouse events
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-                # Redraw the screen during each pass through the loop
-                self.screen.fill(self.settings.bg_color)
-                self.ship.blitme()
+    def _check_events(self):
+        """Respond to keypresses and mousse events"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-                # Make the most recently drawn screen visible.
-                pygame.display.flip()
+    def _update_screen(self):
+        """Update images on the screen, and flip to the new scree"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
+
 
 if __name__=='__main__':
     # Make a game instance, and run the game.
