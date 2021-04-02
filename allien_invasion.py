1b13bf196388b2660af691828696cf548e3c2015
Alberto Macias <alberto.macbar9512@gmail.com>
2021-04-01 19:35:11 -0600

    set color

--- allien_invasion.py
+++ allien_invasion.py
@@ -12,6 +12,9 @@ class AllienInvasion():
         self.screen = pygame.display.set_mode((1000,800))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set the background color
+        self.bg_color = (230,230,230)
+
     def run_game(self):
         """Star the main loop for the game"""
         while True:
@@ -19,6 +22,9 @@ class AllienInvasion():
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+                # Redraw the screen during each pass through the loop
+                self.screen.fill(self.bg_color)
 
                 # Make the most recently drawn screen visible.
                 pygame.display.flip()
