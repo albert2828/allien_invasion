a9309dea0564a58d926a07cd28aa532cd50fa471
Alberto Macias <alberto.macbar9512@gmail.com>
2021-04-02 20:39:38 -0600

    game over added

--- allien_invasion.py
+++ allien_invasion.py
@@ -37,9 +37,12 @@ class AllienInvasion:
         """Star the main loop for the game"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+
             self._update_screen()
 
     def _check_events(self):
@@ -101,16 +104,19 @@ class AllienInvasion:
 
     def _ship_hit(self):
         """Respond to the ship being hit by an alien."""
-        # Decrement ships_left.
-        self.stats.ship_left -= 1
+        if self.stats.ship_left >0:
+            # Decrement ships_left.
+            self.stats.ship_left -= 1
 
-        # Get rid of any remaining aliens and bullets.
-        self.aliens.empty()
-        self.bullets.empty()
+            # Get rid of any remaining aliens and bullets.
+            self.aliens.empty()
+            self.bullets.empty()
 
-        # Create a new fleet and center the ship
-        self._create_fleet()
-        self.ship.center_ship()
+            # Create a new fleet and center the ship
+            self._create_fleet()
+            self.ship.center_ship()
+        else:
+            self.stats.game_active = False
 
         # Pause
         sleep(0.5)
